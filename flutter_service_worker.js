d0c91840696c572f0600b9369fe7643773d627aa
suzanpradhan <sujanpradhan478@gmail.com>
2022-05-18 17:29:29 +0545

    Backend ip added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -401,10 +401,10 @@ const RESOURCES = {
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "icons/maskable_icon_x192.png": "c71b600f7c7ff9640aefc095e03cd3a5",
 "icons/maskable_icon_x512.png": "e46922521ea9088e4a3449a26d1c4d27",
-"index.html": "97827e5797df08a41a9176e275859bd0",
-"/": "97827e5797df08a41a9176e275859bd0",
+"index.html": "e2fe86ac014d2322c03d3108e9538016",
+"/": "e2fe86ac014d2322c03d3108e9538016",
 "logo_colored.png": "71112e1df1fa5f4f73860ace04526e2a",
-"main.dart.js": "2b12bcddaf6a05467873f425e185e73c",
+"main.dart.js": "4c90e028813b17ae00fbdbc64084088c",
 "manifest.json": "d518b72e0f535546f8f4314ac5a9c612",
 "spinner-gainner.gif": "41b53770511b930a1629dea071e0548f",
 "version.json": "4cf1c588c1fbd71522808c4aef5f8dc5"

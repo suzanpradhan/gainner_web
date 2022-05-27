b7eaff0bd72528b03c9e12b3c459c47459687e0d
suzanpradhan <sujanpradhan478@gmail.com>
2022-05-27 15:03:07 +0545

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -386,10 +386,10 @@ const RESOURCES = {
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "icons/maskable_icon_x192.png": "c71b600f7c7ff9640aefc095e03cd3a5",
 "icons/maskable_icon_x512.png": "e46922521ea9088e4a3449a26d1c4d27",
-"index.html": "acfeb4fa498b67a47006ce4c24139eae",
-"/": "acfeb4fa498b67a47006ce4c24139eae",
+"index.html": "f3834fe173e1424c8d431c7824d25347",
+"/": "f3834fe173e1424c8d431c7824d25347",
 "logo_colored.png": "71112e1df1fa5f4f73860ace04526e2a",
-"main.dart.js": "0f8eac9fe46548c002cc5979b0927f65",
+"main.dart.js": "15b13aedbdb0f3cec1a8132595796ec7",
 "manifest.json": "9bfd5201aaa0dba7a12980e4f215ab6b",
 "spinner-gainner.gif": "41b53770511b930a1629dea071e0548f",
 "version.json": "4cf1c588c1fbd71522808c4aef5f8dc5"
